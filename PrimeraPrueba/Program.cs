e47930cc7183faf62c63e4abb9ca0800d27757af
Andrés <anfurtivo1@hotmail.com>
2020-09-23 10:53:13 +0200

    Commit

---
+++ PrimeraPrueba/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeraPrueba
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hola mundo");
+		}
+	}
+}
